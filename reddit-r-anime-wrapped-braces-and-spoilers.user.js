d23558f4895f7f5cce6379f78f8f021a099f9adc
Amndeep Singh Mann <me@asm.works>
2021-10-11 02:43:32 -0400

    1) converted to modifying elements instead of innerHTML and 2) regex is hard so changed algorithm to something that'll be more consistent

--- reddit-r-anime-wrapped-braces-and-spoilers.user.js
+++ reddit-r-anime-wrapped-braces-and-spoilers.user.js
@@ -5,7 +5,7 @@
 // @match       https://www.reddit.com/r/anime/*
 // @match       https://reddit.com/r/anime/*
 // @grant       none
-// @version     1.0
+// @version     2.0
 // @author      asmLANG
 // @description Spoilers are mandated to have braces in front of them.  This script puts those braces in a span and then also wraps the entire spanned braces + spoiler construct with a span.  This enables end users to apply custom css as desired.
 // @updateURL   https://raw.githubusercontent.com/Amndeep7/userscripts/main/reddit-r-anime-wrapped-braces-and-spoilers.user.js
@@ -13,20 +13,60 @@
 // @supportURL  https://github.com/Amndeep7/userscripts/issues
 // ==/UserScript==
 
-(function() {
+const replaceSpoilers = () => {
 	'use strict';
 
+	// find all the spoilers
 	const spoilers = Array.from(document.querySelectorAll('.md-spoiler-text'));
 
-	const parents = new Map();
-	for(const s of spoilers) {
-		parents.set(s.parentNode, parents.has(s.parentNode) ? [...parents.get(s.parentNode), s] : [s]);
+	// process the preceding text to extract the text within brackets so as to place them in a separate span/element from the textnode
+	const previousText = Array.from(spoilers.map(s => s.previousSibling));
+	const braceSpans = [];
+	for(const pt of previousText) {
+		const text = pt.data.trimEnd();
+		let index = text.length - 1;
+		let counter = 0;
+		let braceText = "";
+		do {
+			braceText = text[index] + braceText;
+			switch(text[index]) {
+				case ']':
+					counter += 1;
+					break;
+				case '[':
+					counter -= 1;
+					break;
+				default:
+					break;
+			}
+			index -= 1;
+		} while(counter > 0);
+
+		pt.data = text.slice(0, index + 1); // index is potentally -1 if the entire text is the bracket text
+
+		let span = document.createElement('span');
+		span.classList.add('asmworks-spoiler-braces');
+		let textNode = document.createTextNode(braceText.slice(1, -1)); // remove the braces
+		span.appendChild(textNode);
+		braceSpans.push(span);
 	}
 
-	for(const [p, allS] of parents) {
-		p.innerHTML = p.innerHTML.replaceAll(/\[([\s\S]+?)\]\s?(<span class="md-spoiler-text">)/g, '<span class="asmworks-spoiler-wrapper"><span class="asmworks-spoiler-braces">$1</span>$2');
-		for(const s of allS) {
-			p.innerHTML = p.innerHTML.replace(s.outerHTML, s.outerHTML + '</span>');
-		}
-	}  
+	// modify the DOM
+	for(const [i, s] of spoilers.entries()) {
+		s.classList.add('asmworks-spoiler-spoilertext');
+
+		let wrapper = document.createElement('span');
+		wrapper.classList.add('asmworks-spoiler-wrapper');
+
+		s.parentNode.insertBefore(wrapper, s);
+		wrapper.appendChild(s);
+
+		s.parentNode.insertBefore(braceSpans[i], s);
+	}
+};
+
+(() => {
+	'use strict';
+
+	replaceSpoilers();
 })();
