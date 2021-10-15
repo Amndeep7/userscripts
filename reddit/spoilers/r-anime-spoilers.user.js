e7a63c8745f2ff3aff961e7d264dfd29888b6c6e
Amndeep Singh Mann <me@asm.works>
2021-10-14 21:34:18 -0400

    Restructured repo, fixed the bug where I wasn't properly reverting the changes on a malformed spoiler, created an empty file to hold the css, and updated the documentation a bit

--- reddit/spoilers/r-anime-spoilers.user.js
+++ reddit/spoilers/r-anime-spoilers.user.js
@@ -7,26 +7,13 @@
 // @grant       none
 // @version     4.0
 // @author      asmLANG, gyoex
-// @description Spoilers are mandated to have braces in front of them.  This script puts those braces in a span and then also wraps the entire spanned braces + spoiler construct with a span.  This enables end users to apply custom css as desired.  NOTICE: A breaking change will happen on the next version update where the URLs for update/download will point to different locations.  Please be ready to either manually modify those URLs or reinstall the userscript.
+// @description Spoilers are mandated to have braces in front of them.  This script puts those braces in a span and then also wraps the entire spanned braces + spoiler construct with a span.  This enables end users to apply custom css as desired.
 // @updateURL   https://raw.githubusercontent.com/Amndeep7/userscripts/main/reddit/spoilers/r-anime-spoilers.user.js
 // @downloadURL https://raw.githubusercontent.com/Amndeep7/userscripts/main/reddit/spoilers/r-anime-spoilers.user.js
 // @supportURL  https://github.com/Amndeep7/userscripts/issues
 // ==/UserScript==
 
-/*
- * All this does is 1) add an additional class to the actual spoiler element (`asmworks-spoiler-spoilertext`), 2) separate out the text within the braces, remove the braces, and put them into their own element (`asmworks-spoiler-braces`), 3) wraps the pair of elements (`asmworks-spoiler-wrapper`), and 4) creates some observers that'll do the above to any additional comments that get loaded in.
- *
- * There will be almost no visible impact from using just this userscript by itself besides the braces being removed from the braced text and that same text now abutting the spoiler.  This is intentional as the purpose of this script is to be a foundation for users to style the official reddit spoilers.
- *
- * Use your favorite userscript add-on on your favorite browser to run this, but I can only guarantee it'll run on an up-to-date Firefox with Violentmonkey as the userscript manager.
- *
- * To style these elements, you will need to use something that can apply CSS to a page.  At least two different add-ons can do this for you: 1) RES/Reddit Enhancement Suite (RES settings console -> Appearance -> Stylesheet Loader -> stick the CSS in a snappet) and 2) Stylus (if you're using this, you probably know well enough how to create a new userstyle and apply it to given pages or how to find out).
- *
- * The CSS in these comments in combination with this userscript will make the official reddit spoilers have similar UX to the old /r/anime style:
- * 1) from /u/Durinthal: https://old.reddit.com/r/anime/comments/q28ulr/announcing_changes_to_spoiler_tags/hfjmy9a/
- * 2) from /u/gyoex: https://old.reddit.com/r/anime/comments/pyw36a/casual_discussion_fridays_week_of_october_01_2021/hfkvjpn/
- * 3) from /u/ZaphodBeebblebrox: https://old.reddit.com/r/anime/comments/pyw36a/casual_discussion_fridays_week_of_october_01_2021/hfmpz8v/
- */
+// For more information, see the README for this script at https://github.com/Amndeep7/userscripts/tree/main/reddit/spoilers/.
 
 const replaceSpoilers = (comment) => {
 	'use strict';
@@ -40,7 +27,7 @@ const replaceSpoilers = (comment) => {
 	// process the preceding text and elements so as to extract what is within brackets and place them in a separate span
 	const previousSiblings = Array.from(spoilers.map(s => s.previousSibling));
 	const braceSpans = [];
-	for(const ps of previousSiblings) {
+	for(const [i, ps] of previousSiblings.entries()) {
 		if('classList' in ps && ps.classList.contains('asmworks-spoiler-braces')) {
 			braceSpans.push(null);
 			continue;
@@ -102,6 +89,9 @@ const replaceSpoilers = (comment) => {
 			console.log('Malformed spoiler: does not have braced text');
 			braceSpans.pop();
 			braceSpans.push(null);
+			while(span.firstChild !== null) {
+				spoilers[i].parentNode.insertBefore(span.firstChild, spoilers[i]);
+			}
 			continue;
 		}
 
